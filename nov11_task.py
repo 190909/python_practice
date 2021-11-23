54ac25b6729e4857949965556900595a493b35db
190909 <88303578+190909@users.noreply.github.com>
2021-11-23 13:27:44 +0530

    Create nov11_task.py

---
+++ nov11_task.py
@@ -0,0 +1,26 @@
+#collect string from the user
+a=input("enter the string:  ")
+b=a[len(a)//2]
+print(a)
+print(b)
+
+#convert the middle character to upper case
+a=input("enter the string")
+b=a[len(a)//2]
+print(b.upper())
+
+
+#swapcase
+a=input("enter the string that you have to swap case: ")
+print(a.swapcase())
+
+#mutation
+a=input("enter the first string: ")
+b=input("enter the second string: ")
+c=input("enter the third string: ")
+d= a+b+c
+print(d)
+
+#capitalize
+a=input("enter the string you want to capitalize: ")
+print(a.capitalize())
