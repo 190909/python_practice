84f451a8803c0884b508471f48db425c9d2c72ec
190909 <88303578+190909@users.noreply.github.com>
2021-11-27 22:35:57 +0530

    Update nov_27_task.py

--- nov_27_task.py
+++ nov_27_task.py
@@ -179,5 +179,43 @@ while(n>0):
 print("the sum of digits is: ",total)
 
 
+
+#task16:
+#factors of a number
+n=int(input("enter the number: "))
+factors=[]
+print("the factors of",n,"are" )
+for i in range(1,n+1):
+    if(n%i ==0):
+        factors.append(i)
+print(factors)
+
+
+#task17
+#anagram
+a=input("enter the first string: ")
+b=input("enter the second string: ")
+if(sorted(a)==sorted(b)):
+    print("the strings are anagram")
+else:
+    print("it is not an anagram")
     
+    
+#task18:
+#prime number or not
+a=int(input("enter the number:"))
+if a>1:
+    for i in range(2,int(a/2)+1):
+        if(a%i==0):
+            print(a,"is not a prime")
+            break
+    else:
+        print(a,"is a prime number")
+else:
+    print(a,"is a prime")
+
+    
+    
+   
+
 
